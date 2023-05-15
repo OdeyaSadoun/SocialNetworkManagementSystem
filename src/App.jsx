97023c0c285394d6191fbcef1af9d7d9b0e07304
ori benshushan <orbenshu@g.jct.ac.il>
2023-05-15 23:46:07 +0300

    files changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './Login';
+import Login from './components/Login';
 function App() {
   const [count, setCount] = useState(0)
 
