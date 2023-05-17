461723b78f1b993a7ddfbc0df754d22f4b8644ef
nov segal <novit100@gmail.com>
2023-05-17 13:48:59 +0300

    WORKS WITHOUT BUGS

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Login from './components/Login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import {Link, Route, Routes} from 'react-router-dom'
-
+import Login from './components/Login';
 
 function App() {
-  
   return (
-    <>
+    <Router>
       <Routes>
-        <Route path="/" element= {<HomePage/>}/>
-        <Route path="/login" element= {<Login/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-    </>   
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Login.css';
-const Login = () => {
-  const [userName, setUserName] = useState('');
+function Login (){
+ 
 
   const handleSignUp = (e) => {
     e.preventDefault();
