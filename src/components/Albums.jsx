90bbc93da12d02cbd2bf86341bc98af68a27e3a6
nov segal <novit100@gmail.com>
2023-06-01 17:06:33 +0300

    commiting

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
+function Albums({ userId }) {
+  const [albums, setAlbums] = useState([]);
 
+  useEffect(() => {
+    const fetchAlbums = async () => {
+      try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`);
+        const data = await response.json();
+        setAlbums(data);
+      } catch (error) {
+        console.log('Error fetching albums:', error);
+      }
+    };
 
-function Albums () {
-  // Your component code here
-
+    if (userId) {
+      fetchAlbums();
+    }
+  }, [userId]);
 
   return (
     <>
-    <h1>Albums List</h1>
-    <Link to="/Albums/1">Album 1 </Link>
-    <br/><Link to="/Albums/2">Album 2 </Link>
+      <h1>Albums List</h1>
+      {albums.length > 0 ? (
+        albums.map(album => (
+          <div key={album.id}>
+            <Link to={`/Albums/${album.id}`}>{album.title}</Link>
+          </div>
+        ))
+      ) : (
+        <p>No albums found for the user.</p>
+      )}
     </>
-  )
-};
+  );
+}
 
 export default Albums;
