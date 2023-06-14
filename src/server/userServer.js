896a3896761bce234f3ab7c0f23f852f44b8c79b
אודיה סעדון <osadoun@g.jct.ac.il>
2023-06-14 21:24:18 +0300

    get all users correct

--- src/server/userServer.js
+++ src/server/userServer.js
@@ -1,2 +1,25 @@
 const express = require('express');
+const connection = require('./connection.js');
 const app = express();
+app.use(express.json());
+
+
+// get all users
+app.get('/api/users', (req, res) => {
+  connection.query('SELECT * FROM users', (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query:', err);
+      res.status(500).json({ error: 'Failed to retrieve users' });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
