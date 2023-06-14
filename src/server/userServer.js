099fb2921963191572548e01c458311de2d89169
אודיה סעדון <osadoun@g.jct.ac.il>
2023-06-14 22:23:06 +0300

    Get user by ID fix

--- src/server/userServer.js
+++ src/server/userServer.js
@@ -3,8 +3,26 @@ const connection = require('./connection.js');
 const app = express();
 app.use(express.json());
 
-
-// get all users
+// Get user by ID
+app.get('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    connection.query('SELECT * FROM users WHERE id = ?', [userId], (err, results) => {
+      if (err) {
+        console.error('Error executing MySQL query:', err);
+        res.status(500).json({ error: 'Failed to retrieve user' });
+        return;
+      }
+  
+      if (results.length === 0) {
+        res.status(404).json({ error: 'User not found' });
+        return;
+      }
+  
+      res.json(results[0]);
+    });
+  });
+  
+// Get all users
 app.get('/api/users', (req, res) => {
   connection.query('SELECT * FROM users', (err, results) => {
     if (err) {
@@ -17,6 +35,19 @@ app.get('/api/users', (req, res) => {
 });
 
 
+
+// add a new user
+app.post('/api/users', (req, res) => {
+  const { name, email } = req.body;
+  connection.query('INSERT INTO users (name, email) VALUES (?, ?)', [name, email], (err, result) => {
+    if (err) {
+      console.error('Error executing MySQL query:', err);
+      res.status(500).json({ error: 'Failed to add user' });
+      return;
+    }
+    res.json({ message: 'User added successfully' });
+  });
+});
 
 // Start the server
 const port = 3000;
