3bcd18ea1edd5a0740b4353e98bd1d0d5ac343a1
nov segal <novit100@gmail.com>
2023-06-15 19:16:56 +0300

    node run

--- src/server/connection.js
+++ src/server/connection.js
@@ -1,21 +1,21 @@
-const mysql = require('mysql');
+const mysql = require("mysql2");
 
 // Create MySQL connection
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
+  host: "localhost",
+  user: "root",
+  password: "password",
   port: 3306,
-  database: 'fullStackProject6'
+  database: "FullStackProject6",
 });
 
 // Connect to the MySQL server
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL database:', err);
+    console.error("Error connecting to MySQL database:", err);
     return;
   }
-  console.log('Connected to MySQL database - fullStackProject6');
+  console.log("Connected to MySQL database - fullStackProject6");
 });
 
 module.exports = connection;
