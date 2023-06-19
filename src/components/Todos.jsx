a04d51cc811742edd34ba414f96a4f82121bb395
אודיה סעדון <osadoun@g.jct.ac.il>
2023-06-19 19:51:22 +0300

    more

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -3,51 +3,20 @@ import { userContext } from "../App";
 import { useContext } from "react";
 import RestAPI from "../server/RestAPI"
 
-function Todos() {
-
-  const userId = useContext(userContext).id;
-  const username = useContext(userContext).username;
-
-  const [items, setItems] = useState([]);
+const Todos = () => {
+  const [tasks, setTasks] = useState([]);
   const [sortingCriteria, setSortingCriteria] = useState("");
+  const [items, setItems] = useState([]);
+  const user = JSON.parse(localStorage.getItem('user'));
+  console.log(user);
+  useEffect(() => {
+    const fetchData = async () => {
+      const tasks = await RestAPI.getTodosByUsername(user.username);
+      setTasks(tasks);
+    };
+    fetchData();
+  }, []); // Empty dependency array to run only once
 
-  const fetchTodos = async () => {
-    try {
-      const response = await RestAPI.getTodosByUsername(username);
-      console.log("response", response);
-      const data = response;
-      setItems(data);
-    } catch (error) {
-      console.log("Error fetching todos:", error);
-    }
-  };
-  
-  useEffect(() => { 
-  // Fetch todos when userId changes
-    if (userId) {
-      fetchTodos();
-    }
-  }, []);
-  
- // Toggle completion status of an item
-  const toggleItemCompletion = (id) => {
-    setItems((prevItems) =>
-      prevItems.map((item) => {
-        if (item.id === id) {
-          return { ...item, completed: !item.completed };
-        }
-        return item;
-      })
-    );
-  };
-
-// handles the change event of the sorting criteria select element
-  const handleSortingCriteriaChange = (e) => {
-    setSortingCriteria(e.target.value);
-    sortItems(e.target.value);
-  };
-  
-// Sort items based on the selected criteria
   const sortItems = (criteria) => {
     let sortedItems = [...items];
     switch (criteria) {
@@ -66,14 +35,54 @@ function Todos() {
       default:
         break;
     }
-    setItems(sortedItems);
   };
 
-  const completedStyle = {
-    textDecoration: "line-through",
-    color: "#5a5a63",
-    fontStyle: "italic",
+  
+//  // Toggle completion status of an item
+//   const toggleItemCompletion = (id) => {
+//     setItems((prevItems) =>
+//       prevItems.map((item) => {
+//         if (item.id === id) {
+//           return { ...item, completed: !item.completed };
+//         }
+//         return item;
+//       })
+//     );
+//   };
+
+// handles the change event of the sorting criteria select element
+  const handleSortingCriteriaChange = (e) => {
+    setSortingCriteria(e.target.value);
+    sortItems(e.target.value);
   };
+  
+// // Sort items based on the selected criteria
+//   const sortItems = (criteria) => {
+//     let sortedItems = [...items];
+//     switch (criteria) {
+//       case "serial":
+//         sortedItems.sort((a, b) => a.id - b.id);
+//         break;
+//       case "complited":
+//         sortedItems.sort((a, b) => a.completed - b.completed);
+//         break;
+//       case "alphabetical":
+//         sortedItems.sort((a, b) => a.title.localeCompare(b.title));
+//         break;
+//       case "random":
+//         sortedItems.sort(() => Math.random() - 0.5);
+//         break;
+//       default:
+//         break;
+//     }
+//     setItems(sortedItems);
+//   };
+
+//   const completedStyle = {
+//     textDecoration: "line-through",
+//     color: "#5a5a63",
+//     fontStyle: "italic",
+//   };
 
   return (
     <div className="checklist">
@@ -110,6 +119,6 @@ function Todos() {
       </ul>
     </div>
   );
-}
+        }
 
 export default Todos;
